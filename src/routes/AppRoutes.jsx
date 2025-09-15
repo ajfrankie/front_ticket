750a85fdf72218b0733a7b995dbb384614c06a35
Franklin AJ <franklin@konekt.lk>
2025-09-15 10:58:43 +0530

    update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "../pages/auth/login";
-import Register from "../pages/auth/register";
 import Home from "../pages/home/home";
-import Ticket from "../pages/home/ticket";
-import Track from "../pages/home/track";
-import ViewTicket from "../pages/home/viewticket";
-import UpdateTicket from "../pages/home/updateticket";
-import EditTicket from "../pages/home/editticket";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/ticket" element={<Ticket />} />
-      <Route path="/viewticket" element={<ViewTicket />} />
-      <Route path="/updateticket" element={<UpdateTicket />} />
-      <Route path="/ticket/edit/:ticketId" element={<EditTicket />} />
+      <Route path="/" element={<Home />} />
 
-      <Route path="/track/:id" element={<Track />} />
     </Routes>
   );
 };
